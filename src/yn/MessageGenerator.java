86ba4036a3c8e834c320ad7337df0d487f1cbaa3
Yitzchak Novick <yitzchak.novick@touro.edu>
2024-09-17 20:57:53 -0400

    new messages

--- src/yn/MessageGenerator.java
+++ src/yn/MessageGenerator.java
@@ -4,4 +4,12 @@ public class MessageGenerator {
 	public static String getMessage() {
 		return "Hello Cosmos";
 	}
+	
+	public static String getMessage2() {
+		return "Hi.";
+	}
+	
+	public static String getMessage3() {
+		return "Greetings";
+	}
 }
